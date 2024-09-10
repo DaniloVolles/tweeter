2ea8f1e6f360c1437a36917adc7c80198e120666
Danilo Volles <danilo.volles@outlook.com>
2024-09-10 08:59:01 -0300

    Initial Commit

---
+++ src/test/java/com/danilovolles/tweeterclone/TweeterCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilovolles.tweeterclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeterCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
